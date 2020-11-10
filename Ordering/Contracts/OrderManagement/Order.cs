0ad4c46d04f7ffbf4c6e48e82b291739726761e1
G. B. Karim <george.bekari@gmail.com>
2020-11-10 01:18:06 -0800

    delete ignored files.

--- Ordering/Contracts/OrderManagement/Order.cs
+++ Ordering/Contracts/OrderManagement/Order.cs
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Contracts.OrderManagement
 {
     public class Order
     {
-        //testing
-=======
-﻿namespace Contracts.OrderManagement
-{
-    public class Order
-    {
->>>>>>> main
     }
 }
