22044153ec4f5b605c3b1db79aedf2df5c781426
Jordan Norn <000433959@student.vcc.ca>
2020-11-12 21:24:13 -0800

    Added Order Interface and Order Class

--- Ordering/Contracts/OrderManagement/Order.cs
+++ Ordering/Contracts/OrderManagement/Order.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+
 
 namespace Contracts.OrderManagement
 {
     public class Order
     {
+        public int OrderNumber;
+        public string UserName;
+        public double TotalCost;
+        public DateTime DateOrdered;
+        public SortedList<Object, int> NewOrder;
+
+        public Order(int orderNumber, string userName)
+        {
+            OrderNumber = orderNumber;
+            UserName = userName;
+            TotalCost = 0.00;
+            DateOrdered = DateTime.Now;
+            NewOrder = new SortedList<object, int>();
+        }
+
+        //Need to test order, and modify order framework to make sure it integrates
+
     }
 }
