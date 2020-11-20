eb5e95ea369268068ce85f79b9dadd415ee72ba5
Oswald Kao <oswald.kao@student.vcc.ca>
2020-11-19 20:23:20 -0800

    Fixed problems and added new enum class called OrderItemCategory

--- Ordering/Interfaces/IOrderItem.cs
+++ Ordering/Interfaces/IOrderItem.cs
@@ -1,6 +1,14 @@
-﻿namespace Interfaces
+﻿using System;
+
+namespace Interfaces
 {
     public interface IOrderItem
     {
+        int ItemID { get; }
+        string Name { get; set; }
+        string ItemDescription { get; set; }
+        double Price { get; set; }
+        DateTime DateLastOrdered { get; set; }
+        OrderItemCategory Category { get; set; }
     }
 }
---
+++ Ordering/Interfaces/OrderItemCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public enum OrderItemCategory
+    {
+        Stationary,
+        Computer,
+        Furniture,
+        PersonalProtectiveEquipment,
+        PackingSupplies,
+        FoodandDrink,
+        ComputerAccessories,
+        Software
+    }
+}
