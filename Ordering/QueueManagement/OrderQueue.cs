853e330bf6be4de7a421670905de80ae2c722a87
G. Behzad Karim <bekarim@microsoft.com>
2020-11-27 00:01:45 -0800

    minor updates.

--- Ordering/QueueManagement/OrderQueue.cs
+++ Ordering/QueueManagement/OrderQueue.cs
@@ -2,7 +2,36 @@
 
 namespace QueueManagement
 {
+    // Harman and Abbe : 
+    // I created the IQueue interface for you.
+    // Please implement the Queue constructor and functions 
+    // 
+
     public class OrderQueue<T> : IOrderQueue<T>
     {
+        public OrderQueue()
+        {
+
+        }
+
+        public int Count()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
