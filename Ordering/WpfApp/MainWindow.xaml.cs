f1410b9e758d7c4bf56bde74b8f28a2ccaea1696
G. Behzad Karim <bekarim@microsoft.com>
2020-11-26 17:36:12 -0800

    save work

--- Ordering/WpfApp/MainWindow.xaml.cs
+++ Ordering/WpfApp/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace WpfApp
         {
             InitializeComponent();
         }
+
+        private void TestMe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
