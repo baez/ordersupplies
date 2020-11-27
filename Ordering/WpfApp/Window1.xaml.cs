5f94437e5236ea6fae2bb08789bd4a3ba2b95f0e
G. Behzad Karim <bekarim@microsoft.com>
2020-11-26 19:36:26 -0800

    Added an example XAML page.

---
+++ Ordering/WpfApp/Window1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Contracts.OrderManagement;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+            this.Width = 1000;
+            this.Height = 500;
+            this.Title = "Test Order Entry";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var order = new Order(Convert.ToInt32(OrderNumber.Text), Username.Text);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
