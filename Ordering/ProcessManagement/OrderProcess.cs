a6997845c635d3cf366ca31630ff17565bb66200
Nicole Tan <000431652@student.vcc.ca>
2020-12-10 18:09:25 -0800

    adding notes for orderprocess

--- Ordering/ProcessManagement/OrderProcess.cs
+++ Ordering/ProcessManagement/OrderProcess.cs
@@ -10,6 +10,7 @@ namespace ProcessManagement
 {
     public class OrderProcess<T> : IOrderProcess<T>
     {
+        //remove all not implemented exceptions
         public List<IOrderStep<T>> Steps { get; set; }
         public IOrderStep<T> CurrentStep { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public OrderProcessStatus Status { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
@@ -19,11 +20,17 @@ namespace ProcessManagement
         // add incident number 
         public int IncidentNumber { get; set; }
 
-        public OrderProcess()
+
+        //constructor
+        //set values in the class, required values
+        public OrderProcess(int incidentNumber)
         {
+            //add steps
             Steps = new List<IOrderStep<T>>()
             {
-                new CreateOrderStep<T>()
+                new CreateOrderStep<T>(),
+                new RequestOrder<T>(),
+                new 
             };
         }
     }
