9e831659f15854a901c77f7a221950f86b462d8c
G. Behzad Karim <bekarim@microsoft.com>
2020-12-11 01:17:28 -0800

    Added QueueManager class

---
+++ Ordering/WpfApp/QueueManager.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.OrderManagement;
+using Interfaces.QueueManagement;
+using QueueManagement;
+
+namespace WpfApp
+{
+    public class QueueManager
+    {
+        private IOrderQueue<Order> InitiateOrderQueue;
+
+        public QueueManager()
+        {
+            InitiateOrderQueue = new OrderQueue<Order>();
+        }
+
+        public void AddOrder(Order order)
+        {
+            InitiateOrderQueue.Enqueue(order);
+        }
+    }
+}
